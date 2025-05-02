485c4302dba0c15e2dd94b635504d45baacfd50e
Aaron Cherian <aaron.cherian2@gmail.com>
2025-05-02 10:19:08 -0400

    Update base.py

--- validation/pipeline/base.py
+++ validation/pipeline/base.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
 
     pipe = ValidationPipeline(
         context=ctx,
-        steps=[TemporalAlignmentStep],  # add more as needed
+        steps=[TemporalAlignmentStep], 
         logger=logging.getLogger("pipeline"),
     )
 
