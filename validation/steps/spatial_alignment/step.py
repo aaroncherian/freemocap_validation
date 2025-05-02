93024b2abb2a399cf5af88dccd32c355f9173faa
Aaron Cherian <aaron.cherian2@gmail.com>
2025-05-02 16:32:34 -0400

    add spatial config

--- validation/steps/spatial_alignment/step.py
+++ validation/steps/spatial_alignment/step.py
@@ -1,21 +1,23 @@
 from validation.steps.spatial_alignment.components import REQUIRES, PRODUCES
+from validation.steps.spatial_alignment.config import SpatialAlignmentConfig
+
 from validation.components import QUALISYS_SYNCED_JOINT_CENTERS, FREEMOCAP_ACTOR
 
 
 from validation.pipeline.base import ValidationStep
 
-from pathlib import Path
 
 class SpatialAlignmentStep(ValidationStep):
     REQUIRES = REQUIRES
     PRODUCES = PRODUCES
+    CONFIG = SpatialAlignmentConfig
 
     def calculate(self):
         self.logger.info("Starting spatial alignment")
 
         qualisys_joint_centers = self.data[QUALISYS_SYNCED_JOINT_CENTERS.name]
         freemocap_actor = self.data[FREEMOCAP_ACTOR.name]
-    
+        
     def store(self):
         pass
 
