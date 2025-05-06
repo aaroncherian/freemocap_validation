dfd6642444a11822a17bc76db402519051912004
Aaron Cherian <aaron.cherian2@gmail.com>
2025-05-06 13:55:44 -0400

    rmse kinda working

--- validation/steps/rmse/core/calculate_rmse.py
+++ validation/steps/rmse/core/calculate_rmse.py
@@ -1,6 +1,8 @@
 from skellymodels.experimental.model_redo.managers.human import Human
 from validation.steps.rmse.config import RMSEConfig
+from validation.steps.rmse.core.error_metrics_builder import get_error_metrics
 import numpy as np
+import pandas as pd
 
 def add_velocity_to_actor(human:Human):
     velocity_array = np.diff(human.body.trajectories['3d_xyz'].as_numpy, axis = 0)
@@ -13,7 +15,16 @@ def calculate_rmse(freemocap_actor:Human,
                     config: RMSEConfig):
     f = 2
     #think about using timestamps to get true velocity
-
+    markers_for_comparison = config.markers_for_comparison
     add_velocity_to_actor(freemocap_actor)
     add_velocity_to_actor(qualisys_actor)
+    
+    common_markers_freemocap_df = freemocap_actor.body.trajectories['3d_xyz'].as_dataframe.query('keypoint in @markers_for_comparison')
+    common_markers_freemocap_df['system'] = 'freemocap'
+    common_markers_qualisys_df = qualisys_actor.body.trajectories['3d_xyz'].as_dataframe.query('keypoint in @markers_for_comparison')
+    common_markers_qualisys_df['system'] = 'qualisys'
+
+    combined_df = pd.concat([common_markers_freemocap_df, common_markers_qualisys_df], ignore_index=True)
+    position_error_metrics_dict = get_error_metrics(dataframe_of_3d_data=combined_df)
+
     f = 2
