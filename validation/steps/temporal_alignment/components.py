0de35c4d147fe5d92b37c2d360211b25d0fd8d2a
Aaron Cherian <aaron.cherian2@gmail.com>
2025-09-10 14:32:14 -0400

    Update components.py

--- validation/steps/temporal_alignment/components.py
+++ validation/steps/temporal_alignment/components.py
@@ -2,10 +2,10 @@ from validation.components import (QUALISYS_MARKERS,
                                    QUALISYS_START_TIME, 
                                    QUALISYS_SYNCED_JOINT_CENTERS,
                                    QUALISYS_SYNCED_MARKER_DATA, 
-                                   QUALISYS_COM, 
                                    FREEMOCAP_PRE_SYNC_JOINT_CENTERS, 
                                    FREEMOCAP_TIMESTAMPS,
                                    FREEMOCAP_PREALPHA_TIMESTAMPS,
+                                   FREEMOCAP_LAG
 )
 
 REQUIRES = [FREEMOCAP_TIMESTAMPS, 
@@ -14,10 +14,10 @@ REQUIRES = [FREEMOCAP_TIMESTAMPS,
             FREEMOCAP_PRE_SYNC_JOINT_CENTERS]
 
 
-# REQUIRES = [FREEMOCAP_PREALPHA_TIMESTAMPS, 
-#             QUALISYS_MARKERS, 
-#             QUALISYS_START_TIME,
-#             FREEMOCAP_PRE_SYNC_JOINT_CENTERS]
+REQUIRES = [FREEMOCAP_PREALPHA_TIMESTAMPS, 
+            QUALISYS_MARKERS, 
+            QUALISYS_START_TIME,
+            FREEMOCAP_PRE_SYNC_JOINT_CENTERS]
 
 
-PRODUCES = [QUALISYS_SYNCED_JOINT_CENTERS, QUALISYS_SYNCED_MARKER_DATA]
+PRODUCES = [QUALISYS_SYNCED_JOINT_CENTERS, QUALISYS_SYNCED_MARKER_DATA, FREEMOCAP_LAG]
