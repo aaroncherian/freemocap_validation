da6b5d8ae5a0ac26ccd485b759d8b4bea8be7d59
Aaron Cherian <aaron.cherian2@gmail.com>
2025-12-03 16:23:06 -0500

    flexion yaml

--- validation/pipeline/base.py
+++ validation/pipeline/base.py
@@ -182,7 +182,8 @@ if __name__ == "__main__":
     # cfg_path= Path(r"C:\Users\aaron\Documents\GitHub\freemocap_validation\pipeline_config.yaml")
 
     #JSM treadmill one
-    cfg_path= Path(r"C:\Users\aaron\Documents\GitHub\freemocap_validation\config_yamls\validation\jsm\jsm_treadmill_2.yaml")
+    # cfg_path= Path(r"C:\Users\aaron\Documents\GitHub\freemocap_validation\config_yamls\validation\jsm\jsm_treadmill_2.yaml")
+    cfg_path= Path(r"C:\Users\aaron\Documents\GitHub\freemocap_validation\config_yamls\prosthetic_data\flexion_neutral.yaml")
     
     # path_to_recording = Path(r"D:\2025_09_03_OKK\freemocap\2025-09-03_14-56-30_GMT-4_okk_treadmill_1")
     # cfg_path = Path(r"C:\Users\aaron\Documents\GitHub\freemocap_validation\config_yamls\validation\okk\okk_treadmill_1.yaml")
@@ -195,4 +196,4 @@ if __name__ == "__main__":
         logger=logging.getLogger("pipeline"),
     )
 
-    pipe.run(start_at=2)
+    pipe.run(start_at=0)
