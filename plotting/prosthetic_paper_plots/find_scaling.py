1d7daaa052804d7b445b1f830702e64729f25681
Aaron Cherian <aaron.cherian2@gmail.com>
2026-03-06 11:28:54 -0500

    Update find_scaling.py

--- plotting/prosthetic_paper_plots/find_scaling.py
+++ plotting/prosthetic_paper_plots/find_scaling.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import numpy as np
 
-TRACKER = "rtmpose_dlc"
+TRACKER = "mediapipe"
 
 path_to_recordings = Path(r"D:\2023-06-07_TF01\1.0_recordings\four_camera")
 list_of_folders = [p for p in path_to_recordings.iterdir() if p.is_dir()]
@@ -22,4 +22,8 @@ for valid_folder in list_of_valid_folders:
     scaling_list.append(scaling)
 
 scaling_factor = np.median(scaling_list)
+
+print(f"Median scaling factor across all recordings: {scaling_factor:.4f}")
+print(f"Mean scaling factor across all recordings: {np.mean(scaling_list):.4f}")
+print(f"Standard deviation of scaling factors across all recordings: {np.std(scaling_list):.4f}")
 f = 2
